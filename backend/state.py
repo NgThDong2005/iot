72df55cdd54dae7cf6fef314c0f5ff4292cf9eb9
melyen <thiduyen310@gmail.com>
2025-12-08 16:17:03 +0700

    connect mqtt, subcribe topic temperature, gas

--- backend/state.py
+++ backend/state.py
@@ -15,6 +15,51 @@ from starlette.websockets import WebSocket
 from backend.models import Base
 
 
+import paho.mqtt.client as paho
+from paho.mqtt.client import Client
+from paho import mqtt
+
+
+MQTT_HOST = "646d30de4c774de1b93489fbfb112df9.s1.eu.hivemq.cloud"
+MQTT_PORT = 8883
+MQTT_USER = "fireguardiot6"
+MQTT_PASS = "Demo@123"
+
+def init_mqtt() -> Client :
+	client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
+
+	# setting callbacks for different events to see if it works, print the message etc.
+	def on_connect(client, userdata, flags, rc, properties=None):
+		print("CONNACK received with code %s." %rc)
+	client.on_connect = on_connect
+
+	# with this callback you can see if your publish was successful
+	def on_publish(client, userdata, mid, properties=None):
+		print("mid: " + str(mid))
+	client.on_publish = on_publish
+
+	# print which topic was subscribed to
+	def on_subscribe(client, userdata, mid, granted_qos, properties=None):
+		print("Subscribed: " + str(mid) + " " + str(granted_qos))
+	client.on_subscribe = on_subscribe
+	
+	# print message, useful for checking if it was successful
+	def on_message(client, userdata, msg):
+		print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+	client.on_message = on_message
+
+	client.tls_set( 
+		tls_version=mqtt.client.ssl.PROTOCOL_TLS	
+    )
+
+	client.username_pw_set(MQTT_USER, MQTT_PASS)
+	client.connect(MQTT_HOST, MQTT_PORT)
+	
+	client.loop_start()
+	print("MQTT connected to HiveMQ Cloud!")
+
+	return client
+
 def start_task(
 	callback: Callable[[], Awaitable[None]], interval: float
 ) -> asyncio.Task[None]:
@@ -33,7 +78,10 @@ class AppState:
 
 	ws_connections: set[WebSocket]
 
+	mqtt_client: Client = init_mqtt()
+
 	sensor_task: asyncio.Task[None] | None = None
+
 
 	@classmethod
 	def init(cls, app: Starlette) -> AppState:
@@ -55,6 +103,7 @@ class AppState:
 		)
 
 		app.state.data = state
+
 		return state
 
 	async def deinit(self) -> None:
@@ -86,3 +135,4 @@ class AppState:
 	@staticmethod
 	def get(request: HTTPConnection) -> AppState:
 		return request.app.state.data
+	
